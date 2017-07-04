25b81fc6df89f6d8aef6b1594acf7879877e6de1
todorbalabanov <todorbalabanov@192.168.0.101>
2017-07-04 20:40:13 +0300

    Voxels union all function was added.

--- src/DifferenceBased.java
+++ src/DifferenceBased.java
@@ -4,18 +4,15 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import eu.printingin3d.javascad.context.ColorHandlingContext;
 import eu.printingin3d.javascad.context.IColorGenerationContext;
-import eu.printingin3d.javascad.coords.Abstract3d;
-import eu.printingin3d.javascad.coords.Basic3dFunc;
 import eu.printingin3d.javascad.coords.Coords3d;
 import eu.printingin3d.javascad.coords.Dims3d;
 import eu.printingin3d.javascad.models.Abstract3dModel;
 import eu.printingin3d.javascad.models.Cube;
-import eu.printingin3d.javascad.tranform.ITransformation;
-import eu.printingin3d.javascad.tranform.TransformationFactory;
 import eu.printingin3d.javascad.tranzitions.Colorize;
 import eu.printingin3d.javascad.utils.ModelToFile;
 import eu.printingin3d.javascad.vrl.CSG;
@@ -217,43 +214,43 @@ public class Main {
 	 * All parameters in a single array.
 	 */
 	private static final Object[] PARAMETERS = {
-//			/* One detail with recursive level of one. */
-//			1,
-//			(3),
-//			new byte[][][][]{SIDES_3_PATTERN},
-//			3.0,
-//			+0.001,
-//			1,
-//			new Color[]{Color.WHITE},
+			/* One detail with recursive level of one. */
+			1,
+			(3),
+			new byte[][][][]{SIDES_3_PATTERN},
+			3.0,
+			0.0,
+			1,
+			new Color[]{Color.WHITE},
 			
 //			/* Six details with recursive level of one. */
 //			1,
-//			(3),
+//			(5),
 //			new byte[][][][]{SIDES_3_PATTERN},
 //			3.0,
-//			+0.001,
+//			-0.01,
 //			6,
 //			new Color[]{Color.WHITE, Color.GREEN, Color.RED, Color.WHITE, Color.GREEN, Color.RED},
 			
-//			/* One detail with recursive level of two. */
-//			2,
-//			(4 * 3),
-//			new byte[][][][]{SIDES_4_PATTERN, SIDES_3_PATTERN},
-//			3.0,
-//			+0.001,
-//			1,
-//			new Color[]{Color.WHITE},
-			
 //			/* Six details with recursive level of two. */
 //			2,
-//			(4 * 3),
+//			(5 * 4),
 //			new byte[][][][]{SIDES_4_PATTERN, SIDES_3_PATTERN},
 //			3.0,
 //			-0.01,
 //			6,
 //			new Color[]{Color.WHITE, Color.GREEN, Color.RED, Color.WHITE, Color.GREEN, Color.RED},
+			
+//			/* One detail with recursive level of two. */
+//			2,
+//			(3 * 3),
+//			new byte[][][][]{SIDES_3_PATTERN, SIDES_3_PATTERN},
+//			3.0,
+//			0.0,
+//			1,
+//			new Color[]{Color.WHITE},
 
-//			/* Six details with recursive level of three. */
+//			/* Six details with recursive level of four. */
 //			3,
 //			(5 * 4 * 3),
 //			new byte[][][][]{SIDES_5_PATTERN, SIDES_4_PATTERN, SIDES_3_PATTERN},
@@ -262,8 +259,8 @@ public class Main {
 //			6,
 //			new Color[]{Color.WHITE, Color.GREEN, Color.RED, Color.WHITE, Color.GREEN, Color.RED},
 
-//			/* Six details with recursive level of three. */
-//			3,
+//			/* Six details with recursive level of four. */
+//			4,
 //			(6 * 5 * 4),
 //			new byte[][][][]{SIDES_6_PATTERN, SIDES_5_PATTERN, SIDES_4_PATTERN},
 //			3.0,
@@ -271,23 +268,32 @@ public class Main {
 //			6,
 //			new Color[]{Color.WHITE, Color.GREEN, Color.RED, Color.WHITE, Color.GREEN, Color.RED},
 
-//			/* One detail with recursive level of three. */
-//			3,
+//			/* One detail with recursive level of four. */
+//			4,
 //			(3 * 4 * 5),
 //			new byte[][][][]{SIDES_3_PATTERN, SIDES_4_PATTERN, SIDES_5_PATTERN},
 //			1.0,
-//			+0.001,
+//			-0.01,
 //			1,
 //			new Color[]{Color.WHITE},
 
-			/* One detail with recursive level of four. */
-			4,
-			(3 * 4 * 5 * 6),
-			new byte[][][][]{SIDES_3_PATTERN, SIDES_4_PATTERN, SIDES_5_PATTERN, SIDES_6_PATTERN},
-			1.0,
-			+0.001,
-			1,
-			new Color[]{Color.WHITE},
+//			/* One detail with recursive level of five. */
+//			5,
+//			(3 * 4 * 5 * 6),
+//			new byte[][][][]{SIDES_3_PATTERN, SIDES_4_PATTERN, SIDES_5_PATTERN, SIDES_6_PATTERN},
+//			1.0,
+//			0.0,
+//			1,
+//			new Color[]{Color.WHITE},
+
+//			/* One detail with recursive level of five. */
+//			5,
+//			(3 * 4 * 4 * 5),
+//			new byte[][][][]{SIDES_3_PATTERN, SIDES_4_PATTERN, SIDES_4_PATTERN, SIDES_5_PATTERN},
+//			1.0,
+//			0.0,
+//			1,
+//			new Color[]{Color.WHITE},
 	};
 
 	/**
@@ -311,14 +317,9 @@ public class Main {
 	private static final double VOXEL_SCALE = (Double)PARAMETERS[3];
 	
 	/**
-	 * Space between two voxels. If it is negative there is space, if it is positive there is overlap.
-	 */
-	private static final double VOXEL_DELTA = (Double)PARAMETERS[4];
-	
-	/**
 	 * Single voxel cube side.
 	 */
-	private static final double VOXEL_SIDE = VOXEL_SCALE + VOXEL_DELTA;
+	private static final double VOXEL_SIDE = VOXEL_SCALE + (Double)PARAMETERS[4];
 
 	/**
 	 * How deep recursive calls to be.
@@ -331,37 +332,34 @@ public class Main {
 	private static final Color CONSECUTIVE_DETAILS_COLORS[] = (Color[]) PARAMETERS[6];
 	
 	/**
-	 * 3D space for the shape as discrete voxels.
+	 * 3D fractal shape.
 	 */
-	private static byte voxels[][][] = new byte[SPACE_SIDE_SIZE][SPACE_SIDE_SIZE][SPACE_SIDE_SIZE];
+	private static CSG shape = null;
 
 	/*
 	 * Initialize static members.
 	 */
 	static {
-		for (int x = 0; x < voxels.length; x++) {
-			for (int y = 0; y < voxels[x].length; y++) {
-				for (int z = 0; z < voxels[x][y].length; z++) {
-					voxels[x][y][z] = 1;
-				}
-			}
-		}
 	}
 
 	/**
-	 * Remove unneeded voxels.
+	 * Remove unneeded volume.
 	 * 
 	 * @param sides
 	 *            Coordinates of the sides.
 	 */
-	private static void warp(int[] sides) {
-		for (int x = sides[0]; x <= sides[1]; x++) {
-			for (int y = sides[2]; y <= sides[3]; y++) {
-				for (int z = sides[4]; z <= sides[5]; z++) {
-					voxels[x][y][z] = 0;
-				}
-			}
+	private static void warp(double[] sides) {
+		double side = Math.max(sides[1]-sides[0], Math.max(sides[3]-sides[2], sides[5]-sides[4]));
+		
+		/*
+		 * Shape with zero size in maximum dimension can not be removed. 
+		 */
+		if(side == 0) {
+			return;
 		}
+		
+		Cube cube = new Cube(side);
+		shape = shape.difference((cube.move(new Coords3d(sides[0], sides[2], sides[4]))).toCSG());
 	}
 
 	/**
@@ -372,7 +370,7 @@ public class Main {
 	 * @param sides
 	 *            Coordinates of the sides.
 	 */
-	private static void cube(int level, int sides[]) {
+	private static void cube(int level, double sides[]) {
 		/*
 		 * End of details level.
 		 */
@@ -381,17 +379,22 @@ public class Main {
 		}
 
 		int length = SIDES_PATTERNS[level-1][0].length;
-		for (int x = sides[0], dx = (sides[1] - sides[0] + 1) / length; x <= sides[1]; x += dx) {
-			int a = (x - sides[0]) / (dx);
-			for (int y = sides[2], dy = (sides[3] - sides[2] + 1) / length; y <= sides[3]; y += dy) {
-				int b = (y - sides[2]) / (dy);
-				for (int z = sides[4], dz = (sides[5] - sides[4] + 1) / length; z <= sides[5]; z += dz) {
-					int c = (z - sides[4]) / (dz);
+		double dx = (sides[1] - sides[0]) / length;
+		double dy = (sides[3] - sides[2]) / length;
+		double dz = (sides[5] - sides[4]) / length;
+		
+		double x, y, z;
+		for (int a = 0; a < length; a++) {
+			x = sides[0] + a * dx;
+			for (int b = 0; b < length; b++) {
+				y = sides[2] + b * dy;
+				for (int c = 0; c < length; c++) {
+					z = sides[4] + c * dz;
 
 					if (SIDES_PATTERNS[level-1][a][b][c] == 1) {
-						cube(level - 1, new int[] { x, x + dx - 1, y, y + dy - 1, z, z + dz - 1 });
+						cube(level - 1, new double[] { x, x + dx, y, y + dy, z, z + dz });
 					} else if (SIDES_PATTERNS[level-1][a][b][c] == 0) {
-						warp(new int[] { x, x + dx - 1, y, y + dy - 1, z, z + dz - 1 });
+						warp(new double[] { x, x + dx, y, y + dy, z, z + dz });
 					}
 				}
 			}
@@ -403,114 +406,12 @@ public class Main {
 	 * 
 	 * @return Array of values with the amount of empty space, amount of occupied space and total space.
 	 */
-	private static int[] volume() {
-		int counters[] = {0, 0, 0};
+	private static double[] volume() {
+		double counters[] = {0, 0, 0};
 		
-		for (int x = 0; x < voxels.length; x++) {
-			for (int y = 0; y < voxels[x].length; y++) {
-				for (int z = 0; z < voxels[x][y].length; z++) {
-					if(voxels[x][y][z] == 0) {
-						counters[0]++;
-					} else if(voxels[x][y][z] == 1) {
-						counters[1]++;
-					}
-					
-					counters[2]++;
-				}
-			}
-		}
+		//TODO Volume calculation is different when CSG object is used.
 		
 		return counters;
-	}
-	
-	/**
-	 * Transform voxels to 3D model in order to be saved as STL.
-	 * 
-	 * @return Complex 3D model.
-	 */
-	private static Abstract3dModel voxelsToModel() {
-		Abstract3dModel model = new Cube(new Dims3d(voxels.length, voxels[0].length, voxels[0][0].length));
-		
-		boolean first = true;
-		for (int x = 0; x < voxels.length; x++) {
-			for (int y = 0; y < voxels[x].length; y++) {
-				for (int z = 0; z < voxels[x][y].length; z++) {
-					if (voxels[x][y][z] == 0) {
-						continue;
-					}
-					
-					if(first == true) {
-						model = new Cube(VOXEL_SIDE).move(new Coords3d(x*VOXEL_SCALE, y*VOXEL_SCALE, z*VOXEL_SCALE));
-						first = false;
-					} else {
-						model = model.addModel((new Cube(VOXEL_SIDE)).move(new Coords3d(x*VOXEL_SCALE, y*VOXEL_SCALE, z*VOXEL_SCALE)));
-					}
-				}
-			}
-		}
-		
-		Abstract3dModel group = new Colorize(CONSECUTIVE_DETAILS_COLORS[0], model);
-		for(int i=1; i<NUMBER_OF_CONSECUTIVE_DETAILS; i++) {
-			group = group.addModel( new Colorize(CONSECUTIVE_DETAILS_COLORS[i], model.move(new Coords3d(0, 0, i*SPACE_SIDE_SIZE*VOXEL_SCALE))) );
-		}
-		model = group;
-		
-		return model;
-	}
-
-	/**
-	 * Transform voxels to CSG data in order to be saved as STL.
-	 * 
-	 * @return Complex 3D model.
-	 */
-	private static CSG voxelsToCSG() {
-		List<CSG> shapes = new ArrayList<CSG>();
-
-		/*
-		 * Transform all voxels in list of CSG cubes.
-		 */
-		for (int x = 0; x < voxels.length; x++) {
-			for (int y = 0; y < voxels[x].length; y++) {
-				for (int z = 0; z < voxels[x][y].length; z++) {
-					if (voxels[x][y][z] == 0) {
-						continue;
-					}
-					
-					shapes.add((new Cube(VOXEL_SIDE).move(new Coords3d(x*VOXEL_SCALE, y*VOXEL_SCALE, z*VOXEL_SCALE))).toCSG());
-				}
-			}
-		}
-		
-		/*
-		 * Union shape by shape.
-		 */
-		loop: for(int i=0; i<shapes.size(); i++) {
-			for(int j=i+1; j<shapes.size(); j++) {
-				CSG union = shapes.get(i).union(shapes.get(j));
-				if(union.toFacets().size() == 0) {
-					continue;
-				}
-				
-				/*
-				 * Union of two shapes.
-				 */
-				shapes.remove(j);
-				shapes.remove(i);
-				shapes.add(0, union);
-				i = j = 0;
-				continue loop;
-			}
-		}
-		
-		CSG model = shapes.get(0).union(shapes.get(1));
-		
-		CSG group = shapes.get(0).union(shapes.get(1));
-		for(int i=1; i<NUMBER_OF_CONSECUTIVE_DETAILS; i++) {
-			group = group.union(model.transformed(TransformationFactory.getTranlationMatrix(new Coords3d(0, 0, i*SPACE_SIDE_SIZE*VOXEL_SCALE-VOXEL_DELTA))));
-		}
-		model = group;
-
-		return model;
 	}
 	
 	/**
@@ -524,19 +425,21 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		System.out.println("Start ...");
 
-		cube(RECURSIVE_DEPTH_LEVEL, new int[] { 0, SPACE_SIDE_SIZE - 1, 0, SPACE_SIDE_SIZE - 1, 0, SPACE_SIDE_SIZE - 1 });
+		Cube solid = new Cube(SPACE_SIDE_SIZE*VOXEL_SCALE);
+		shape = solid.toCSG();
+		cube(RECURSIVE_DEPTH_LEVEL, new double[] { 	solid.getBoundaries().getX().getMin(), solid.getBoundaries().getX().getMax(), 
+													solid.getBoundaries().getY().getMin(), solid.getBoundaries().getY().getMax(), 
+													solid.getBoundaries().getZ().getMin(), solid.getBoundaries().getZ().getMax() });
 
-		System.out.println("Volume: " + Arrays.toString(volume()));
-		
+		//System.out.println("Volume: " + Arrays.toString(volume()));
+
 		/*
 		 * Use STL Java library for file saving.
 		 */
 		StlBinaryFile out = new StlBinaryFile( new FileOutputStream("./cube" + System.currentTimeMillis() + ".stl") );
-		//out.writeToFile(voxelsToModel().toCSG().toFacets());
-		out.writeToFile(voxelsToCSG().toFacets());
+		out.writeToFile(shape.toFacets());
 		out.close();
-		
-		//ModelToFile out = new ModelToFile( new File("./bin/cube" + System.currentTimeMillis() + ".scad") );
+		//ModelToFile out = new ModelToFile( new File("./cube" + System.currentTimeMillis() + ".scad") );
 		//out.addModel( voxelsToModel() ).saveToFile( ColorHandlingContext.DEFAULT );
 		
 		System.out.println("Stop ...");
