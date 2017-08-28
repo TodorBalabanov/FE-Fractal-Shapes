a5cbc6338674c9086485c685e967bb974bb4e3e4
Todor Balabanov <todor.balabanov@gmail.com>
2017-08-28 11:06:15 +0300

    Configuration for complex cube.

--- src/Main.java
+++ src/Main.java
@@ -186,14 +186,14 @@ public class Main {
 			// SIDES_3_PATTERN }, 1.0, +0.001, 1,
 			// new Color[] { Color.WHITE }, 1,
 
-			/* One detail with recursive level of three. */
-			3, (3 * 5 * 7), new byte[][][][] { SIDES_3_PATTERN, SIDES_5_PATTERN, SIDES_7_PATTERN }, 1.0, +0.001, 1,
-			new Color[] { Color.WHITE }, 1,
+//			/* One detail with recursive level of three. */
+//			3, (3 * 5 * 7), new byte[][][][] { SIDES_3_PATTERN, SIDES_5_PATTERN, SIDES_7_PATTERN }, 1.0, +0.001, 1,
+//			new Color[] { Color.WHITE }, 1,
 
-			// /* One detail with recursive level of four. */
-			// 4, (3 * 4 * 5 * 6), new byte[][][][] { SIDES_3_PATTERN, SIDES_4_PATTERN,
-			// SIDES_5_PATTERN, SIDES_6_PATTERN },
-			// 1.0, +0.001, 1, new Color[] { Color.WHITE }, 1,
+			 /* One detail with recursive level of four. */
+			 4, (3 * 4 * 5 * 6), new byte[][][][] { SIDES_3_PATTERN, SIDES_4_PATTERN,
+			 SIDES_5_PATTERN, SIDES_6_PATTERN },
+			 1.0, +0.001, 1, new Color[] { Color.WHITE }, 1,
 
 			// /* One detail with recursive level of four. */
 			// 4, (6 * 5 * 4 * 3), new byte[][][][] { SIDES_6_PATTERN, SIDES_5_PATTERN,
